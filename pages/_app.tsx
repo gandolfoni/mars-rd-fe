398352a20e560a7806c493e970c3f5f65f35279b
Your Name <gandolfoni@protonmail.com>
2025-01-02 23:09:05 -0500

    style: standardize import quotes in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
